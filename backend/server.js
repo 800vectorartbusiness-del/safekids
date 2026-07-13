0ab47788e280393ce5c3259196a734bcbed71295
Creative Bharat <800vectorartbusiness@gmail.com>
2026-07-13 07:06:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -58,7 +58,7 @@ const Media = mongoose.model('Media', mediaSchema);
 // ========= MULTER SETUP =========
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-    cb(null, uploadPath); // ✅ absolute path use karo
+    cb(null, upload); // ✅ absolute path use karo
 },
     filename: function (req, file, cb) {
         const uniqueName = Date.now() + '-' + file.originalname;
